c22b64f70a42c9384e6e096646e5654c48af54f9
yogesh17kumar <165543133+yogesh17kumar@users.noreply.github.com>
2026-02-16 15:28:28 +0530

    Add files via upload

---
+++ Pattern/HollowPyramid.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class HollowPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter no of rows: ");
+        int n = sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=2*i-1;j++){
+                if (j==1||j==2*i-1||i==n){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern/HollowSquare.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
