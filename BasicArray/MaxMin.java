976904ef25b229320a8cd60fb82c96f72af5aa3a
yogesh17kumar <165543133+yogesh17kumar@users.noreply.github.com>
2026-03-10 20:35:30 +0530

    Add files via upload

---
+++ BasicArray/MaxMin.java
@@ -0,0 +1,28 @@
+package BasicArray;
+
+import java.util.Scanner;
+
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i<n;i++){
+            if(arr[i]>max){
+                    max = arr[i];
+            }
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Maximum element: "+max);
+        System.out.println("Minimum element: "+min);
+    }
+}
