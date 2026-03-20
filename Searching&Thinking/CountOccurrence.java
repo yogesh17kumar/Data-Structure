2af875caa02d7729e95aa0ed3bb9a8d6c4ee1324
Yogeshh <165543133+yogesh17kumar@users.noreply.github.com>
2026-03-20 16:30:31 +0530

    Add files via upload

---
+++ Searching&Thinking/CountOccurrence.java
@@ -0,0 +1,9 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class CountOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
