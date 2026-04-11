9f216abedfe67c4a7dccc30ae41e75479edccf6c
Yogeshh <165543133+yogesh17kumar@users.noreply.github.com>
2026-04-11 11:30:07 +0530

    Add files via upload

---
+++ BitManipulation/MoveNegative.java
@@ -0,0 +1,50 @@
+package BitManipulation;
+
+import java.util.*;
+
+public class MoveNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input elements
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left <= right) {
+            if(arr[left] < 0) {
+                left++;
+            }
+            else if(arr[right] >= 0) {
+                right--;
+            }
+            else {
+                // swap
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // Output result
+        System.out.println("Array after moving negatives to left:");
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
